6ddc8a9e55f7a086abc88195819fd7de9a2f09a2
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2025-07-21 23:00:36 -0300

    Updates

--- _app/immutable/chunks/CGjwlU5S.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_u6ax5j?.base??"",a=globalThis.__sveltekit_u6ax5j?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/DTdkSFxn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1kn9ec5?.base??"/pamsz",e=globalThis.__sveltekit_1kn9ec5?.assets??s;export{e as a,s as b};
