fd43b22b6b546a59d0b79b761de5beb8d845796e
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2025-07-21 22:05:15 -0300

    mvp completo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../global.css'
+    import '@picocss/pico/css/pico.css';
+</script>
+<main class="container">
+    <slot/>
+</main>
+
+<style>
+    main {
+        padding-top: 3rem;
+    }
+</style>
